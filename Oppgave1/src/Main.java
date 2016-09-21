593166e5e0fae088b507a263694306904009d8b3
staaleho <staaleho@student.matnat.uio.no>
2016-09-21 14:05:31 +0200

    dede

--- Oppgave1/src/Main.java
+++ Oppgave1/src/Main.java
@@ -5,67 +5,70 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
+
     public static void main(String[] args) throws Exception{
+        /*
+        Initial setup for tree and other variables.
+        Creates input and output file.
+         */
         File words = new File("words.txt");
         Scanner s = new Scanner(words);
+
+        //Uses first word to create a node.
         Node root = new Node(s.nextLine());
         System.out.println("Root made");
         Tree tree = new Tree(root);
-        ArrayList<String> inputarraylist = new ArrayList<String>();
+        ArrayList<String> treestatistics = new ArrayList<String>();
+        ArrayList<String> results = new ArrayList<String>();
+
+        File utskrift = new File("utskrift.txt");
+        utskrift.createNewFile();
+        FileWriter fw = new FileWriter(utskrift);
+
+        //Reads the file, creates nodes, and sorts them.
 
         while(s.hasNextLine()){
-
             Node newnode = new Node(s.nextLine());
             root.sortNode(newnode);
         }
-        long total = 0;
+        long total = tree.getTotal();
+        treestatistics = tree.treeStatistics();
 
+        //Writes statistics for the tree to the output file.
+        for (String treestring: treestatistics) {
+            fw.write(treestring);
+            fw.write(System.getProperty("line.separator"));
+            fw.flush();
+        }
+        fw.write(System.getProperty("line.separator"));
 
-
-
-
-
-        System.out.println("Welcome to spellcheck! Enter word:");
+        //Initiates spellcheck.
+        System.out.println("Welcome to spellcheck! Enter word: ('q' to exit)");
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
-        while(!input.equals("q")){
-            if(!root.findValue(input, root)){
-                System.out.println("Not found! Trying spellcheck:");
-                inputarraylist = tree.wordGenerator(input);
-                System.out.println("There are " + inputarraylist.size() + " variants of the word to try.");
-                long startTime = System.currentTimeMillis();
-                for (String str: inputarraylist) {
-                    if(root.findValue(str, root)){
-                        System.out.println("Found " + str + " instead of " + input);
-                    }
+        while(!input.equals("q")) {
+            if (!root.findValue(input, root)) {
+                results = tree.spellCheck(input);
+                for (String rstring: results) {
+                    fw.write(rstring);
+                    fw.flush();
+                    fw.write(System.getProperty("line.separator"));
+                    fw.flush();
                 }
-                long endTime = System.currentTimeMillis();
-                System.out.println("It took " + (endTime - startTime) + " ms to search for alternatives.");
+                fw.write(System.getProperty("line.separator"));
+                System.out.println("Enter word: ('q' to exit)" );
+                input = in.nextLine();
             }else{
-                System.out.println("Word found!");
+                System.out.println(input + " found!");
+                System.out.println("Enter word: ('q' to exit)");
+                input = in.nextLine();
             }
-            input = in.nextLine();
+
+            results.clear();
         }
 
-        System.out.println("Some statistics about the current tree:");
-        System.out.println("1: The tree has a depth of " + tree.getDepth(root));
-        System.out.println("2: Per level, there are the follow number of nodes:");
-        tree.nodesPerLevel();
-        System.out.println("3: The average depth is " + Math.log(tree.getTotal()) / Math.log(2) + ".");
-        System.out.print("4: The first word is " + root.findMaximumValue(root).getValue());
-        System.out.println(", and the last is " + root.findMinimumValue(root).getValue() + ".");
-
-
-
-
-
-
-
-
+        for (String exitstring : treestatistics) {
+            System.out.println(exitstring);
+        }
     }
-
-
-    
-
-
 }
